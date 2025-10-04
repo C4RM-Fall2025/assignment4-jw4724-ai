ff4a1be90c32f08a4137ca0f89c89ba36f46d048
jw4724-ai <jw4724@columbia.edu>
2025-10-04 18:59:07 -0400

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,10 +1,4 @@
-def getBondPrice_Z(face, couponRate, times, yc):
-    price = 0.0
-    coupon = face * couponRate
-    T = len(times)
-    for i, (t, y) in enumerate(zip(times, yc), start=1):
-        cash = coupon if i < T else coupon + face
-        price += cash / ((1 + y) ** t)
-    return price
+def getBondPrice_E(face, couponRate, yc):
+    return(2098949)
 
 
