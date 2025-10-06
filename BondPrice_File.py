0c214d6a1bd1ebfbdb24a1ec7f5ad2ca8719527d
jw4724-ai <jw4724@columbia.edu>
2025-10-06 00:41:05 -0400

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,6 +1,11 @@
 def getBondPrice(y, face, couponRate, m, ppy=1):
-    if ppy == 1:
-        return(2170604)
-    elif ppy == 2:
-        return(2171686)
-
+    if ppy <= 0:
+        ppy = 1
+    r = y / ppy
+    n = int(m * ppy)
+    c = face * couponRate / ppy
+    price = 0.0
+    for t in range(1, n + 1):
+        price += c / ((1 + r) ** t)
+    price += face / ((1 + r) ** n)
+    return price
